b9b00ade2250000f89961efc114b1b8e77ed7747
N.O.Atomusk <orzkey@outlook.com>
2022-12-15 14:27:54 +0800

    为远程方案添加tensorboard支持

--- scripts/set-lemonade.sh
+++ scripts/set-lemonade.sh
@@ -2,7 +2,7 @@
 
 if [ "$(cat /etc/hostname)" = "Noatomusk" ]; then
     lemonade server -allow 127.0.0.1 --log-level=4 &
-    ssh -R 2489:127.0.0.1:2489 -p 43968 zk@10.133.30.71
+    ssh -L 43969:127.0.0.1:43969 -R 2489:127.0.0.1:2489 -p 43968 zk@10.133.30.71
 else
     lemonade server -allow 127.0.0.1 --log-level=4 &
     ssh -R 2489:127.0.0.1:2489 -p 43968 zk@119.29.90.39
