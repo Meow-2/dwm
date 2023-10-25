b7b62f471d4b6ffd17b7f1ec61483d15689eff1d
N.O.Atomusk <orzkey@outlook.com>
2023-10-25 18:13:39 +0800

    autossh自动重连

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -35,7 +35,7 @@ settings() {
         # autossh -M 4396 -NR 43970:localhost:43968 zk@119.29.90.39 -p 43968 &
         # 转发到宿舍
         source ~/.config/zsh/env.zsh
-        autossh -M 4396 -NR 43970:localhost:43968 zk@${HOME_IP} -p 43968 &
+        autossh -M 4396 -fCNR 43970:localhost:43968 zk@${HOME_IP} -p 43968 &
     fi
 }
 
