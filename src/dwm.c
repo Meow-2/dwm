b2fd2d4798bd333bc9aaaed648785b438e61c845
N.O.Atomusk <orzkey@outlook.com>
2024-08-15 08:59:04 +0800

    :sparkles:鼠标移动切换屏幕时自动获取屏幕焦点

--- src/dwm.c
+++ src/dwm.c
@@ -223,7 +223,7 @@ static void drawbar(Monitor *m);
 static void drawbars(void);
 static int drawstatusbar(Monitor *m, int bh, char* text);
 
-// static void enternotify(XEvent *e);
+static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 
 static void focusin(XEvent *e);
@@ -366,7 +366,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
     [ConfigureRequest] = configurerequest,
     [ConfigureNotify] = configurenotify,
     [DestroyNotify] = destroynotify,
-    // [EnterNotify] = enternotify,
+    [EnterNotify] = enternotify,
     [Expose] = expose,
     [FocusIn] = focusin,
     [KeyPress] = keypress,
@@ -1253,24 +1253,25 @@ drawstatusbar(Monitor *m, int bh, char* stext) {
     return status_w - 2;
 }
 
-// void
-// enternotify(XEvent *e)
-// {
-//     Client *c;
-//     Monitor *m;
-//     XCrossingEvent *ev = &e->xcrossing;
-//
-//     if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
-//         return;
-//     c = wintoclient(ev->window);
-//     m = c ? c->mon : wintomon(ev->window);
-//     if (m != selmon) {
-//         unfocus(selmon->sel, 1);
-//         selmon = m;
-//     } else if (!c || c == selmon->sel)
-//         return;
-//     focus(c);
-// }
+void
+enternotify(XEvent *e)
+{
+    Client *c;
+    Monitor *m;
+    XCrossingEvent *ev = &e->xcrossing;
+
+    if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+        return;
+    c = wintoclient(ev->window);
+    m = c ? c->mon : wintomon(ev->window);
+    if (m != selmon) {
+        unfocus(selmon->sel, 1);
+        selmon = m;
+    }
+    // } else if (!c || c == selmon->sel)
+    //     return;
+    // focus(c);
+}
 
 void
 expose(XEvent *e)
