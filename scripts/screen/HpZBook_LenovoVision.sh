b3b1d2ae94f70556740c81868e8bde8138f105dc
zk <orzkey@outlook.com>
2024-10-16 09:15:03 +0800

    config:修改多显示器排列位置

--- scripts/screen/HpZBook_LenovoVision.sh
+++ scripts/screen/HpZBook_LenovoVision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP-1 --mode 1920x1080 --rate 60.00 --pos 1920x1080 --rotate normal \
+xrandr --output eDP-1 --mode 1920x1080 --rate 60.00 --pos 1920x0 --rotate normal \
     --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rate 60.00 --scale 1x1 --rotate normal \
     --output DP-1 --off \
     --output DP-2 --off \
